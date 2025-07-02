1ae9b89975618b5b57e26bb81c5001844f113b12
Shubhangan Das <shubhangandas7366@gmail.com>
2025-07-03 00:07:31 +0530

    chunks2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 1000 // increases limit from default 500kB
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          ui: ['@mui/material', '@emotion/react', '@emotion/styled'], // example if you're using MUI
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000
   }
 })
+
