f17f81df1276d5b2378c6aaf3b104d991261c4f7
Shubhangan Das <shubhangandas7366@gmail.com>
2025-07-03 00:03:46 +0530

    chunks

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000 // increases limit from default 500kB
+  }
 })
